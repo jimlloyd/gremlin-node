bf81e399e99969323c5fcaedee96b7a73524e7d6
Jim Lloyd <jim.lloyd@gmail.com>
2014-12-03 12:15:33 -0800

    Rework toJSON to be conformant with JSON package.
    We had methods named toJSON that were async. This interferred with
    the workings of JSON.stringify. In the course of fixing this problem
    it became clear that we could remove a significant amount of code
    that essentially duplicated portions of JSON.stringify.

--- test/test-gremlin.js
+++ test/test-gremlin.js
@@ -2,10 +2,8 @@
 
 var _ = require('lodash');
 var assert = require('assert');
-var sinon = require('sinon');
 var Gremlin = require('../lib/gremlin');
 var GraphWrapper = require('../lib/graph-wrapper');
-var VertexWrapper = require('../lib/vertex-wrapper');
 var EdgeWrapper = require('../lib/edge-wrapper');
 
 suite('gremlin', function () {
@@ -21,105 +19,6 @@ suite('gremlin', function () {
     var TinkerGraphFactory = gremlin.java.import('com.tinkerpop.gremlin.tinkergraph.structure.TinkerFactory');
     graph = TinkerGraphFactory.createClassicSync();
     g = new GraphWrapper(gremlin, graph);
-  });
-
-  test('Wrapped objects can be converted to JS objects using gremlin.toJSON', function (done) {
-    g.v(2, function (err, v) {
-      assert.ifError(err);
-      assert(v instanceof VertexWrapper);
-      gremlin.toJSON(v, function (err, json) {
-        assert.ifError(err);
-        assert.strictEqual(json.id, 2);
-        done();
-      });
-    });
-  });
-
-  test('Wrapped objects can be converted to JS objects using gremlin.toJSONSync', function (done) {
-    g.v(2, function (err, v) {
-      assert.ifError(err);
-      assert(v instanceof VertexWrapper);
-      var json = gremlin.toJSONSync(v);
-      assert.strictEqual(json.id, 2);
-      done();
-    });
-  });
-
-  test('gremlin.toJSON returns simple vertex by default', function (done) {
-    g.v(2, function (err, v) {
-      assert.ifError(err);
-      assert(v instanceof VertexWrapper);
-      gremlin.toJSON(v, function (err, json) {
-        assert.ifError(err);
-        var expected = {
-          id: 2,
-          label: 'vertex',
-          type: 'vertex',
-          properties: {name: 'vadas', age: 27}
-        };
-        assert.deepEqual(json, expected);
-        done();
-      });
-    });
-  });
-
-  test('gremlin.toJSON(keepHiddens) returns full tinkerpop3 vertex', function (done) {
-    g.v(2, function (err, v) {
-      assert.ifError(err);
-      assert(v instanceof VertexWrapper);
-      gremlin.toJSON(v, { keepHiddens: true }, function (err, json) {
-        assert.ifError(err);
-        var expected = { id: 2,
-          label: 'vertex',
-          type: 'vertex',
-          hiddens: {},
-          properties: { name: 'vadas', age: 27 }
-        };
-        assert.deepEqual(json, expected);
-        done();
-      });
-    });
-  });
-
-  test('gremlin.toJSON(strict) returns full tinkerpop3 vertex', function (done) {
-    g.v(2, function (err, v) {
-      assert.ifError(err);
-      assert(v instanceof VertexWrapper);
-      gremlin.toJSON(v, { strict: true }, function (err, json) {
-        assert.ifError(err);
-        var expected = { id: 2,
-          label: 'vertex',
-          type: 'vertex',
-          hiddens: {},
-          properties:
-           { name:
-              [ { id: 2,
-                  label: 'name',
-                  hiddens: {},
-                  value: 'vadas',
-                  properties: {} } ],
-             age: [ { id: 3, label: 'age', hiddens: {}, value: 27, properties: {} } ] }
-        };
-        assert.deepEqual(json, expected);
-        done();
-      });
-    });
-  });
-
-  test('gremlin.toJSON returns null when passed null', function (done) {
-    gremlin.toJSON(null, function (err, json) {
-      assert.ifError(err);
-      assert.strictEqual(json, null);
-      done();
-    });
-  });
-
-  test('gremlin.toJSON returns undefined when passed undefined', function (done) {
-    gremlin.toJSON(undefined, function (err, json) {
-      assert.ifError(err);
-      assert.strictEqual(json, undefined);
-      done();
-    });
   });
 
   test('gremlin.toList(jsarray) using callback API', function (done) {
